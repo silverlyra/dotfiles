a8d852b6c65b24e5f2610101146473c588fed2d2
Lyra Naeseth <lyra@lyra.codes>
2021-07-30 00:06:48 -0700

    Update fzf zsh module for Arch

--- zsh/zmodules/fzf.zsh
+++ zsh/zmodules/fzf.zsh
@@ -1,7 +1,9 @@
-if [[ -d /opt/homebrew/opt/fzf ]]; then
-  # Auto-completion
-  [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+for fzfs in /opt/homebrew/opt/fzf/shell /usr/share/fzf; do
+  if [[ -d "$fzfs" ]]; then
+    # Auto-completion
+    [[ $- == *i* ]] && source "${fzfs}/completion.zsh" 2> /dev/null
 
-  # Key bindings
-  source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
-fi
+    # Key bindings
+    source "${fzfs}/key-bindings.zsh"
+  fi
+done
