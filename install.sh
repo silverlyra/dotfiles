d7a8c9f6dcfc138555e454efb98d1d6ba562c183
Lyra Naeseth <lyra@lyra.codes>
2021-10-29 13:37:30 -0700

    Install antibody automatically

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 apply() {
   local src="$(pwd)/$1"
   local dest="${HOME}/.$(basename "$1")"
@@ -14,3 +16,9 @@ mkdir -p ~/.config
 ln -fns "$(pwd)/alacritty" ~/.config/alacritty
 ln -fns "$(pwd)/nvim" ~/.config/nvim
 ln -fns "$(pwd)/tmux" ~/.config/tmux
+
+if ! command -v antibody >/dev/null; then
+  # Install Antibody (zsh plugin manager)
+  mkdir -p ~/.local/bin
+  curl -fsSL https://git.io/antibody | sh -s - -b ~/.local/bin
+fi
