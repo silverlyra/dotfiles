27cc2d8347451d6cd613d5dbafff53432568a3e8
Lyra Naeseth <lyra@lyra.codes>
2021-10-29 13:34:46 -0700

    Add alacritty, tmux config

--- install.sh
+++ install.sh
@@ -11,4 +11,6 @@ apply zsh/zmodules
 apply zsh/zshrc
 
 mkdir -p ~/.config
+ln -fns "$(pwd)/alacritty" ~/.config/alacritty
 ln -fns "$(pwd)/nvim" ~/.config/nvim
+ln -fns "$(pwd)/tmux" ~/.config/tmux
